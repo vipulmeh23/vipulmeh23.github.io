7439ef67455b941442955cdfaea22cd3254a6565
Vipul Mehra <vipulmeh23@gmail.com>
2023-08-20 03:04:35 -0700

    Removed BMI cURL, added bmi calculator function instead. Stale API removed.

--- HandsOn/bmi/curl/curl.php
+++ HandsOn/bmi/curl/curl.php
@@ -1,5 +1,4 @@
 <?php
-    $url = "https://bmi.p.mashape.com/";
     $age = $_GET["age"];
     $weight = $_GET["weight"];
     $unitWeight = $_GET["unitW"];
@@ -7,34 +6,36 @@
     $unitHeight = $_GET["unitH"];
     $sex = $_GET["sex"];
 
-    $data = array(
-        "weight"=>array(
-            "value"=>$weight,
-            "unit"=>$unitWeight ),
-        "height"=>array(
-            "value"=>$height,
-            "unit"=>$unitHeight),
-        "sex"=> $sex,
-        "age"=>$age,
-        "waist"=>"",
-        "hip"=>""
-    );
-    $dataJson = json_encode($data);
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $dataJson);
-    curl_setopt($ch, CURLOPT_POST, 1);
+    function calculateBMI($age, $weight, $height, $unitHeight, $unitWeight) {
+        $parsedUnitHeight = $unitHeight;
+        $parsedUnitWeight = $unitWeight;
+        $parsedHeight = (float)$height;
+        $parsedWeight = (float)$weight;
+        
+        if($parsedUnitHeight == "cm"){
+            $parsedHeight = $parsedHeight/100;
+        }
 
-    $headers = array();
-    $headers[] = "X-Mashape-Key: 60MsMLqcJvmsh5qD46X74dyjHaEKp1RqRDNjsnrAwYUzRzWu0P";
-    $headers[] = "Content-Type: application/json";
-    $headers[] = "Accept: application/json";
-    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        if($parsedUnitWeight == "lb"){
+            $parsedWeight = $parsedWeight/2.205;
+        }
 
-    $result = curl_exec($ch);
-    if (curl_errno($ch)) {
-        echo 'Error:' . curl_error($ch);
+        $bmi = $parsedWeight/($parsedHeight * $parsedHeight);
+
+        return $bmi;
+
     }
-    curl_close ($ch);
-    echo $result;
+
+    $calculatedBMI = calculateBMI($age, $weight, $height, $unitHeight, $unitWeight);
+
+    $weightStatus = "Normal";
+
+    if($calculatedBMI < 18.5){
+        $weightStatus = "Underweight";
+    } elseif($calculatedBMI > 25 && $calculatedBMI < 40) {
+        $weightStatus = "Obese";
+    }
+
+    $resultArr = array("bmi" => array("value" => $calculatedBMI, "status" => $weightStatus));
+    
+    echo json_encode($resultArr);
